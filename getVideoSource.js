7573d7e6a6ea717fd8394143c99dc4485d24f4a9
Trần Trung Hậu <trunghau.t1@gmail.com>
2017-09-17 17:00:10 +0700

    Update function

--- getVideoSource.js
+++ getVideoSource.js
@@ -1,29 +1,45 @@
 // ==UserScript==
 // @name         Get Video Source
 // @namespace    https://github.com/trunghaut1/GetVideoSource
-// @version      0.2
+// @version      0.4
 // @description  Get original video from online movie sites
 // @author       TrungHau
 // @include      https://phimbathu.com/xem-phim/*
 // @include      http://bilutv.com/xem-phim/*
+// @include      http://tvhay.org/xem-phim-*
+// @updateURL    https://raw.githubusercontent.com/trunghaut1/GetVideoSource/master/getVideoSource.js
+// @downloadURL  https://raw.githubusercontent.com/trunghaut1/GetVideoSource/master/getVideoSource.js
 // @grant        none
+// @grant          unsafeWindow
+// @grant          GM_xmlhttpRequest
+// @grant          GM_addStyle
+// @grant          GM_getResourceText
+// @grant          GM_getResourceURL
+// @grant          GM_deleteValue
+// @grant          GM_getValue
+// @grant          GM_openInTab
+// @grant          GM_registerMenuCommand
+// @grant          GM_setValue
 // @require      https://apis.google.com/js/api.js
+// @require      http://code.jquery.com/jquery-1.11.0.min.js
 // @homepage     https://trunghaut1.github.io/GetVideoSource/
 // ==/UserScript==
 
 function getDriveInfo(driveId) {
-    gapi.client.init({
-        apiKey: 'AIzaSyBUq_Gd1KCL7ix7asMNs9SKrEqyxOEmIE8'
-    }).then(function(){
-        var request = gapi.client.request({
-            'path': '/drive/v2/files/'+ driveId,
-            'method': 'GET',
-        });
-        request.execute(function(resp) {
-            if(resp.title != null)
-                $("#downLink").html(resp.mimeType + " | " + resp.videoMediaMetadata.width + "x" + resp.videoMediaMetadata.height + " | " +
-                                    resp.createdDate.substring(0, 19) + " | " + parseInt(resp.fileSize/1024/1024) + " MB");
-            else $("#downLink").html("Lỗi truy cập file | " + driveId);
+    return new Promise(function(resolve, reject) {
+        gapi.client.init({
+            apiKey: 'AIzaSyBUq_Gd1KCL7ix7asMNs9SKrEqyxOEmIE8'
+        }).then(function(){
+            var request = gapi.client.request({
+                'path': '/drive/v2/files/'+ driveId,
+                'method': 'GET',
+            });
+            request.execute(function(resp) {
+                if(resp.title !== undefined) {
+                    resolve(resp);
+                }
+                else reject("Lỗi truy cập file | " + driveId);
+            });
         });
     });
 }
@@ -32,29 +48,53 @@ function checkSource()
 {
     var check = $(".jw-video").length;
     if(check === 0) return "open";
+    else if(!$(".jw-video").attr("src").includes('google')) return "other";
     return "drive";
+}
+function getDriveLink(source) {
+    return new Promise((resolve, reject) =>
+                      $.get('http://quiztool.azurewebsites.net/getvideo.php', {driveUrl : source})
+                           .done(resolve).fail(reject));
 }
 function getVideo() {
     var source = null;
+    //
+    var getDriveVideo = function(source) {
+        if(source !== null && source.includes('google')) {
+            var url = new URL(source);
+            var driveId = url.searchParams.get("driveid");
+            if(driveId !== null) {
+                $("#downLink").attr("href","https://drive.google.com/file/d/" + driveId + "/view");
+                gapi.load('client', function(){ getDriveInfo(driveId).then(function(info) {
+                    var time = new Date(info.createdDate);
+                    $("#downLink").html(info.mimeType + " | " + info.videoMediaMetadata.width + "x" + info.videoMediaMetadata.height + " | " +
+                                        time.toLocaleString() + " | " + parseInt(info.fileSize/1024/1024) + " MB");
+                }, error => $("#downLink").html(error));});
+            } else $("#downLink").html("Không tìm thấy Google Drive ID, vui lòng thử lại");
+        }
+        else $("#downLink").html("Không tìm thấy liên kết, vui lòng thử lại");
+    };
+    
     switch(checkSource()) {
-        case "drive" :
+        case "drive" : // Get Google Drive source
             {
                 source = $(".jw-video").attr("src");
-                if(source != null) {
-                    var url = new URL(source);
-                    var driveId = url.searchParams.get("driveid");
-                    if(driveId != null) {
-                        $("#downLink").attr("href","https://drive.google.com/file/d/" + driveId + "/view");
-                        gapi.load('client', function(){getDriveInfo(driveId);});
-                    }
+                if(source !== null && source.includes('googleusercontent')) {
+                    getDriveLink(source).then(function(newUrl) {
+                        source = newUrl;
+                        getDriveVideo(source);
+                    });
                 }
-                else $("#downLink").html("Không tìm thấy liên kết");
+                else getDriveVideo(source);
                 break;
             }
-        case "open" :
+        case "open" : // Get Openload source
             {
-                source = $("#player").find("iframe").attr("src");
-                if(source != null) {
+                if(location.href.indexOf("tvhay.org") > 0)
+                    source = $("#media").find("iframe").attr("src");
+                else
+                    source = $("#player").find("iframe").attr("src");
+                if(source !== null && source.includes('openload')) {
                     var fileId = source.substring(26);
                     $("#downLink").attr("href","https://openload.co/f/" + fileId);
                     $.get(
@@ -68,25 +108,54 @@ function getVideo() {
                         }
                     );
                 }
-                else $("#downLink").html("Không tìm thấy liên kết");
+                else if(source !== null) {
+                    $("#downLink").attr("href",source);
+                    $("#downLink").html(source);
+                }
+                else $("#downLink").html("Không tìm thấy liên kết, vui lòng thử lại");
                 break;
+            }
+            default :
+            {
+                source = $(".jw-video").attr("src");
+                var leng = source.length;
+                if(leng > 80)
+                    $("#downLink").html(source.substring(0,leng/10) + "..." + source.substring(leng-5));
+                else
+                    $("#downLink").html(source.substring(0,leng/2) + "..." + source.substring(leng-10));
+                $("#downLink").attr("href",source);
             }
     }
 }
 
 $(document).ready(function(){
-    // Add download button
+    //Create download button
     var downBtn = document.createElement("a");
     downBtn.setAttribute("id","downBtn");
     downBtn.setAttribute("class", "btn btn-sm btn-primary");
     downBtn.innerHTML = "Tải phim";
-    $(".option.col-lg-10.col-md-10.col-sm-8.col-xs-8").first().append(downBtn);
-    $("#downBtn").click(getVideo);
-
-    // Add link download button
+    //Create link download button
     var downLink = document.createElement("a");
     downLink.setAttribute("id","downLink");
     downLink.setAttribute("class", "btn btn-sm btn-info");
     downLink.style.cssText = "float:right;";
-    $(".option.col-lg-10.col-md-10.col-sm-8.col-xs-8").first().append(downLink);
+    downLink.setAttribute("target","_blank");
+    //Check site
+    if(location.href.indexOf("phimbathu.com") > 0 || location.href.indexOf("bilutv.com") > 0) {
+        //Add download button
+        $(".option.col-lg-10.col-md-10.col-sm-8.col-xs-8").first().append(downBtn);
+        $("#downBtn").click(getVideo);
+        //Add link download button
+        $(".option.col-lg-10.col-md-10.col-sm-8.col-xs-8").first().append(downLink);
+    } else
+    if(location.href.indexOf("tvhay.org") > 0) {
+        // Add download button
+        $(".action").append(downBtn);
+        $("#downBtn").click(getVideo);
+        //Add link download button
+        downLink.setAttribute("class", "btn btn-primary");
+        downLink.style.cssText = "top:0; position: absolute; right: 0; height:16px; margin-top:-3px; margin-right:10px;";
+        $("#detail").find(".blockbody").attr("style","position: relative;");
+        $("#detail").find(".blockbody").append(downLink);
+    }
 });
